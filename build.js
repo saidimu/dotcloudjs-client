cdcf4ef436814c5034b34f586073037616364bd1
shin- <joffrey@dotcloud.com>
2012-04-25 16:25:02 -0700

    Improved builder that also supports stack-specific config.js files

--- build.js
+++ build.js
@@ -1,4 +1,5 @@
-var fs = require('fs');
+var fs = require('fs'),
+    util = require('util');
 
 var DEFAULT_CONFIG = {
     paths: {
@@ -15,8 +16,24 @@ function conf_string(cfg_obj) {
     return 'require.config(' + JSON.stringify(cfg_obj) + ');';
 }
 
+function copy(src, dst, cb) {
+    is = fs.createReadStream(src);
+    os = fs.createWriteStream(dst);
+    util.pump(is, os, cb);
+}
+
+function read_json(path) {
+    var contents = null;
+    try {
+        contents = JSON.parse(fs.readFileSync(process.argv[3], 'utf-8'));
+    } catch (e) {
+        console.error('File should obey the JSON format.', e);
+    }
+    return contents;
+}
+
 function usage() {
-    console.error('Usage: node build.js [-f config.json]');
+    console.error('Usage: node build.js [[-s STACK_VARS] -f require_config.json]');
 }
 
 function build(cfg_obj) {
@@ -26,17 +43,41 @@ function build(cfg_obj) {
         conf_string(cfg_obj));
 }
 
+function copy_src_files() {
+    copy('./src/dotcloud.js', './build/dotcloud.js');
+    copy('./libraries/jq-cookie.js', './build/jq-cookie.js');
+}
+
 if (argc === 2) { // No additional arguments, default build
     build(DEFAULT_CONFIG);
 } else if (process.argv[2] == '-f' && argc === 4) {
-    var cfg = null;
-    try {
-        cfg = JSON.parse(fs.readFileSync(process.argv[3]));
-    } catch (e) {
-        console.error('File should obey the JSON format.', e);
+    var cfg = read_json(fs.readFileSync(process.argv[3]));
+    if (!cfg) 
+        process.exit(1);
+    else
+        build(cfg);
+} else if (process.argv[2] == '-s' && argc >= 4) {
+    if (argc === 4) {
+        build({ paths: { jquery: DEFAULT_CONFIG.paths.jquery }});
+    } else if (process.argv[4] == '-f' && argc >= 6) {
+        var cfg = read_json(fs.readFileSync(process.argv[5]));
+        if (!cfg)
+            process.exit(1);
+        else
+            build(cfg);
+    } else {
+        usage();
         process.exit(1);
     }
-    build(cfg);
+    copy_src_files();
+    var configjs = fs.readFileSync('./src/config.js.TMPL', 'utf-8');
+    var config_vars = JSON.parse(process.argv[3]);
+    for (var k in config_vars) {
+        var tmpl_key = '/**' + k.toUpperCase() + '**/';
+        configjs = configjs.replace(tmpl_key, '"' + config_vars[k] + '"');
+    }
+    fs.writeFileSync('./build/config.js', configjs);
 } else {
     usage();
+    process.exit(1);
 }
