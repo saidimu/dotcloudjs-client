4a5024c61b01008cbaee117d526dc489959d4ac1
shin- <joffrey@dotcloud.com>
2012-06-05 17:45:07 -0700

    Added OAuth support

--- src/twitter.js
+++ src/twitter.js
@@ -10,17 +10,24 @@
 define(function(require) {
     return function(config) {
         var io = require('stack.io')(),
-            appService = null;
-        var transfer = function(name, args) {
+            appService = null, accessToken = null;
+        var transfer = function(name, args, noauth) {
             if (!appService) throw 'Twitter service was not initialized.';
+            args = Array.prototype.slice.call(args);
+            noauth || args.unshift(accessToken);
+            console.log(name + ": ", args);
             io.call(appService, name).apply(undefined, args);
         }
 
         var module = {
             init: function(key, secret, token, cb) {
+                if (typeof token == 'function') {
+                    cb = token, token = null;
+                }
+
                 io.call('twitter', 'init')(key, secret, token, function(svcName) {
                     appService = svcName;
-                    cb();
+                    cb && cb();
                 });
             },
             
@@ -126,16 +133,16 @@ define(function(require) {
 
             // ### Block
             blocking: function(params, ids, cb) {
-                transfer('blocking', [params, ids, cb]);
+                transfer('blocking', arguments);
             },
             isBlocked: function(user, params, cb) {
-                transfer('isBlocked', [user, params, cb]);
+                transfer('isBlocked', arguments);
             },
             createBlock: function(user, params, cb) {
-                transfer('createBlock', [user, params, cb]);
+                transfer('createBlock', arguments);
             },
             destroyBlock: function(user, params, cb) {
-                transfer('destroyBlock', [user, params, cb]);
+                transfer('destroyBlock', arguments);
             },
 
             // ### Favorites
@@ -151,7 +158,7 @@ define(function(require) {
 
             // ### Lists
             allLists: function(user, cb) {
-                transfer('allLists', [user, cb]);
+                transfer('allLists', arguments);
             },
 
             listStatuses: function(list, owner, params, cb) {
@@ -220,13 +227,13 @@ define(function(require) {
 
             // ### Accounts
             rateLimit: function(cb) {
-                transfer('rateLimit', [cb]);
+                transfer('rateLimit', arguments);
             },
             verifyCredentials: function(cb) {
-                transfer('verifyCredentials', [cb]);
+                transfer('verifyCredentials', arguments);
             },
             endSession: function(cb) {
-                transfer('endSession', [cb]);
+                transfer('endSession', arguments);
             },
             updateProfile: function(params, cb) {
                 transfer('updateProfile', arguments);
@@ -238,71 +245,71 @@ define(function(require) {
                 transfer('updateProfileImg', arguments);
             },
             updateProfileColors: function(params, cb) {
-                transfer('updateProfileColors', [params, cb]);
+                transfer('updateProfileColors', arguments);
             },
 
             accountTotals: function(cb) {
-                transfer('accountTotals', [cb]);
+                transfer('accountTotals', arguments);
             },
 
             settings: function(cb) {
-                transfer('settings', [cb]);
+                transfer('settings', arguments);
             },
 
             updateSettings: function(params, cb) {
-                transfer('updateSettings', [params, cb]);
+                transfer('updateSettings', arguments);
             },
 
             // ### Notifications
             follow: function(user, cb) {
-                transfer('follow', [user, cb]);
+                transfer('follow', arguments);
             },
             leave: function(user, cb) {
-                transfer('leave', [user, cb]);
+                transfer('leave', arguments);
             },
             // ### Saved searches
             savedSearches: function(cb) {
-                transfer('savedSearches', [cb]);
+                transfer('savedSearches', arguments);
             },
             showSavedSearch: function(id, cb) {
-                transfer('showSavedSearch', [id, cb]);
+                transfer('showSavedSearch', arguments);
             },
             createSavedSearch: function(query, cb) {
-                transfer('createSavedSearch', [query, cb]);
+                transfer('createSavedSearch', arguments);
             },
             destroySavedSearch: function(id, cb) {
-                transfer('destroySavedSearch', [id, cb]);
+                transfer('destroySavedSearch', arguments);
             },
 
             // ### Geolocation
             geoId: function(placeId, cb) {
-                transfer('geoId', [placeId, cb]);
+                transfer('geoId', arguments);
             },
             reverseGeocode: function(latitude, longitude, params, cb) {
-                transfer('reverseGeocode', [latitude, longitude, params, cb]);
+                transfer('reverseGeocode', arguments);
             },
             searchGeo: function(query, params, cb) {
-                transfer('searchGeo', [query, params, cb]);
+                transfer('searchGeo', arguments);
             },
             similarPlaces: function(latitude, longitude, name, params, cb) {
                 transfer('similarPlaces', arguments);
             },
             createPlace: function(params, cb) {
-                transfer('createPlace', [params, cb]);
+                transfer('createPlace', arguments);
             },
 
             // ### Trends
             trends: function(woeid, exclude, cb) {
-                transfer('trends', [woeid, exclude, cb]);
+                transfer('trends', arguments);
             },
             availableTrends: function(latitude, longitude, cb) {
-                transfer('dailyTrends', [latitude, longitude, cb]);
+                transfer('dailyTrends', arguments);
             },
             dailyTrends: function(params, cb) {
-                transfer('dailyTrends', [params, cb]);
+                transfer('dailyTrends', arguments);
             },
             weeklyTrends: function(params, cb) {
-                transfer('weeklyTrends', [params, cb]);
+                transfer('weeklyTrends', arguments);
             },
 
             // ### Help
@@ -310,32 +317,82 @@ define(function(require) {
                 transfer('test', [cb]);
             },
             config: function(cb) {
-                transfer('config', [cb]);
+                transfer('config', [cb], true);
             },
             languages: function(cb) {
-                transfer('languages', [cb]);
+                transfer('languages', [cb], true);
             },
 
             // ### Search API
-            search: function() {
-                transfer('search', arguments);
+            search: function(query, params, cb) {
+                transfer('search', arguments, true);
             },
 
             // ## Streaming API
             sampleStream: function(params, cb) {
-                transfer('sampleStream', [params, cb]);
+                transfer('sampleStream', arguments);
             },
             filteredStream: function(params, cb) {
-                transfer('filteredStream', [params, cb]);
+                transfer('filteredStream', arguments);
             },
             firehose: function(params, cb) {
-                transfer('firehose', [params, cb]);
+                transfer('firehose', arguments);
             },
             siteStream: function(follow, params, cb) {
-                transfer('siteStream', [follow, params, cb]);
+                transfer('siteStream', arguments);
             },
             userStream: function(params, cb) {
-                transfer('userStream', [params, cb]);
+                transfer('userStream', arguments);
+            },
+
+            // ## OAuth
+            requestToken: function(cb) {
+                transfer('requestToken', [config.host, cb], true);
+            },
+            accessToken: function(requestToken, secret, cb) {
+                if (!appService) throw 'Twitter service was not initialized.';
+
+                var listener = function(e) {
+                    if (e.origin !== config.host)
+                        return;
+                    var data = JSON.parse(e.data);
+
+                    if (data.token != requestToken) {
+                        return cb({ error: 'Received token is different from sent token' });
+                    }
+
+                    io.call(appService, 'accessToken')(data.verifier, data.token, secret, function(result) {
+                        if (!result.error) {
+                            accessToken = { key: result.key, secret: result.secret };
+                        }
+                        cb(result);
+                    });
+                    window.removeEventListener('message', listener);
+                };
+                window.addEventListener('message', listener);
+                window.open('https://api.twitter.com/oauth/authenticate?oauth_token=' + 
+                    requestToken);
+            },
+            auth: function(cb) {
+                // FIXME: check if cookie present before going through the whole flow.
+                var self = this;
+                this.requestToken(function(result) {
+                    if (result.error) {
+                        return cb(result);
+                    }
+                    self.accessToken(result.token, result.secret, function(r2) {
+                        if (r2.error) {
+                            return cb(r2);
+                        }
+                        $.cookie('dotcloudjs-twitter.' + r2.appKey, 
+                            JSON.stringify(r2), { expires: 9001, path: '/' });
+                        accessToken = { key: r2.key, secret: r2.secret };
+                        cb(r2);
+                    });
+                });
+            },
+            setAccessToken: function(key, secret) {
+                accessToken = { key: key, secret: secret };
             }
         };
 
